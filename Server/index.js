a6be204b635ea2ea48b7dda26e8416ecadfd92c7
DevvMaurya <mauryadevv@gmail.com>
2024-02-08 14:46:25 +0530

    renamed some files for deployment puepose

--- Server/index.js
+++ Server/index.js
@@ -42,7 +42,7 @@ app.get('/apidata', (req, res) => {
     res.send('Listening..!');
 });
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

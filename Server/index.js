06ec555ed98391bde80868d25fffde6b370d1218
DevvMaurya <mauryadevv@gmail.com>
2024-02-08 15:33:42 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Enable CORS
 const corsOptions = {
-    origin: '*', // Change this to your specific frontend origin
+    origin: 'https://lost-found-kappa.vercel.app/', // Replace with your frontend URL
     credentials: true,
     optionSuccessStatus: 200,
 }
@@ -22,24 +22,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Session middleware
 app.use(
     session({
-        secret: 'your-secret-key', // Change this to a secure key
+        secret: 'lost and found', // Change this to a secure key
         resave: false,
         saveUninitialized: true,
-        cookie: { secure: false }, // Change to true in production for HTTPS
+        cookie: { secure: true }, // Change to true in production for HTTPS
     })
 );
-
-
-app.use('/',(req,res)=>{
-    console.log('connected...!')
-    res.json({"msg" : 'connected'});
-})
 
 // Routes from router.js
 app.use('/', router);
 
-app.get('/apidata', (req, res) => {
-    res.send('Listening..!');
+// Handle non-existent routes
+app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' });
 });
 
 const PORT = process.env.PORT || 4000;
